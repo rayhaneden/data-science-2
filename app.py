25954ab640d51deb54032fdd7eb4b097791e3282
rayhaneden <rayhaneden@gmail.com>
2025-05-25 18:59:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,93 @@
 import streamlit as st
 import pandas as pd
 import joblib
-from data_preprocessing import data_preprocessing
-from prediction import prediction
+
 
 import streamlit as st
 import pandas as pd
+
+# Load setiap scaler secara manual
+pca_1 = joblib.load("model/pca_1.joblib")
+scaler_Application_mode = joblib.load("model/scaler_Application_mode.joblib")
+scaler_Previous_qualification_grade = joblib.load("model/scaler_Previous_qualification_grade.joblib")
+scaler_Admission_grade = joblib.load("model/scaler_Admission_grade.joblib")
+scaler_Displaced = joblib.load("model/scaler_Displaced.joblib")
+scaler_Debtor = joblib.load("model/scaler_Debtor.joblib")
+scaler_Tuition_fees_up_to_date = joblib.load("model/scaler_Tuition_fees_up_to_date.joblib")
+scaler_Gender = joblib.load("model/scaler_Gender.joblib")
+scaler_Scholarship_holder = joblib.load("model/scaler_Scholarship_holder.joblib")
+scaler_Age_at_enrollment = joblib.load("model/scaler_Age_at_enrollment.joblib")
+scaler_Curricular_units_1st_sem_enrolled = joblib.load("model/scaler_Curricular_units_1st_sem_enrolled.joblib")
+scaler_Curricular_units_1st_sem_evaluations = joblib.load("model/scaler_Curricular_units_1st_sem_evaluations.joblib")
+scaler_Curricular_units_1st_sem_approved = joblib.load("model/scaler_Curricular_units_1st_sem_approved.joblib")
+scaler_Curricular_units_1st_sem_grade = joblib.load("model/scaler_Curricular_units_1st_sem_grade.joblib")
+scaler_Curricular_units_2nd_sem_enrolled = joblib.load("model/scaler_Curricular_units_2nd_sem_enrolled.joblib")
+scaler_Curricular_units_2nd_sem_evaluations = joblib.load("model/scaler_Curricular_units_2nd_sem_evaluations.joblib")
+scaler_Curricular_units_2nd_sem_approved = joblib.load("model/scaler_Curricular_units_2nd_sem_approved.joblib")
+scaler_Curricular_units_2nd_sem_grade = joblib.load("model/scaler_Curricular_units_2nd_sem_grade.joblib")
+
+
+
+# Daftar kolom untuk PCA
+pca_numerical_columns = [
+    'Curricular_units_1st_sem_enrolled', 'Curricular_units_1st_sem_evaluations',
+    'Curricular_units_1st_sem_approved', 'Curricular_units_1st_sem_grade',
+    'Curricular_units_2nd_sem_enrolled', 'Curricular_units_2nd_sem_evaluations',
+    'Curricular_units_2nd_sem_approved', 'Curricular_units_2nd_sem_grade'
+]
+
+def data_preprocessing(data):
+    """Preprocessing data
+
+    Args:
+        data (Pandas DataFrame): Dataframe yang berisi semua data untuk prediksi
+
+    Returns:
+        Pandas DataFrame: Dataframe yang berisi semua data yang sudah dipreprocess
+    """
+    data = data.copy()
+    df = pd.DataFrame()
+    # Standarisasi kolom selain PCA (manual)
+    df["Application_mode"] = scaler_Application_mode.transform(np.asarray(data["Application_mode"]).reshape(-1,1))[0]
+    df["Previous_qualification_grade"] = scaler_Previous_qualification_grade.transform(np.asarray(data["Previous_qualification_grade"]).reshape(-1,1))[0]
+    df["Admission_grade"] = scaler_Admission_grade.transform(np.asarray(data["Admission_grade"]).reshape(-1,1))[0]
+    df["Displaced"] = scaler_Displaced.transform(np.asarray(data["Displaced"]).reshape(-1,1))[0]
+    df["Debtor"] = scaler_Debtor.transform(np.asarray(data["Debtor"]).reshape(-1,1))[0]
+    df["Tuition_fees_up_to_date"] = scaler_Tuition_fees_up_to_date.transform(np.asarray(data["Tuition_fees_up_to_date"]).reshape(-1,1))[0]
+    df["Gender"] = scaler_Gender.transform(np.asarray(data["Gender"]).reshape(-1,1))[0]
+    df["Scholarship_holder"] = scaler_Scholarship_holder.transform(np.asarray(data["Scholarship_holder"]).reshape(-1,1))[0]
+    df["Age_at_enrollment"] = scaler_Age_at_enrollment.transform(np.asarray(data["Age_at_enrollment"]).reshape(-1,1))[0]
+
+    # Standarisasi kolom PCA (untuk diproses PCA)
+    data["Curricular_units_1st_sem_enrolled"] = scaler_Curricular_units_1st_sem_enrolled.transform(np.asarray(data["Curricular_units_1st_sem_enrolled"]).reshape(-1,1))[0]
+    data["Curricular_units_1st_sem_evaluations"] = scaler_Curricular_units_1st_sem_evaluations.transform(np.asarray(data["Curricular_units_1st_sem_evaluations"]).reshape(-1,1))[0]
+    data["Curricular_units_1st_sem_approved"] = scaler_Curricular_units_1st_sem_approved.transform(np.asarray(data["Curricular_units_1st_sem_approved"]).reshape(-1,1))[0]
+    data["Curricular_units_1st_sem_grade"] = scaler_Curricular_units_1st_sem_grade.transform(np.asarray(data["Curricular_units_1st_sem_grade"]).reshape(-1,1))[0]
+    data["Curricular_units_2nd_sem_enrolled"] = scaler_Curricular_units_2nd_sem_enrolled.transform(np.asarray(data["Curricular_units_2nd_sem_enrolled"]).reshape(-1,1))[0]
+    data["Curricular_units_2nd_sem_evaluations"] = scaler_Curricular_units_2nd_sem_evaluations.transform(np.asarray(data["Curricular_units_2nd_sem_evaluations"]).reshape(-1,1))[0]
+    data["Curricular_units_2nd_sem_approved"] = scaler_Curricular_units_2nd_sem_approved.transform(np.asarray(data["Curricular_units_2nd_sem_approved"]).reshape(-1,1))[0]
+    data["Curricular_units_2nd_sem_grade"] = scaler_Curricular_units_2nd_sem_grade.transform(np.asarray(data["Curricular_units_2nd_sem_grade"]).reshape(-1,1))[0]
+
+    # PCA (5 komponen)
+    df[["pc1_1", "pc1_2", "pc1_3", "pc1_4", "pc1_5"]] = pca_1.transform(data[pca_numerical_columns].values.reshape(1, -1))
+
+    return df
+
+model = joblib.load("model/best_rfl.joblib")
+result_target = joblib.load("model/encoder_target.joblib")
+def prediction(data):
+    """Making prediction
+ 
+    Args:
+        data (Pandas DataFrame): Dataframe that contain all the preprocessed data
+ 
+    Returns:
+        str: Prediction result (Good, Standard, or Poor)
+    """
+    result = model.predict(data)
+    final_result = result_target.inverse_transform(result)[0]
+    return final_result
+
 
 application_mode_mapping = {
     1: "1st phase - general contingent",
